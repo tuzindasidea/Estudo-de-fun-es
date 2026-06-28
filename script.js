ac4d2c5b8b597eda2242d4af9462af85e831112b
tuzindasidea <tuzindasidea123@gmail.com>
2026-06-28 13:12:20 -0300

    Atualização função quadrática

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuBtn = document.getElementById('menuBtn');
+const fecharBtn = document.getElementById('fecharBtn');
+const sidebar = document.getElementById('sidebar');
+
+// Função para abrir
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.add('ativa');
+});
+
+ 
+const fecharMenu = () => {
+    sidebar.classList.remove('ativa');
+};
+
+fecharBtn.addEventListener('click', fecharMenu);
+overlay.addEventListener('click', fecharMenu); 
